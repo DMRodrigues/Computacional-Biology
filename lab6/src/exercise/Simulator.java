d57bbfba37e5c6201c66fed83387f87934f62ade
Nuno Pires <nuno.gomes.pires@tecnico.ulisboa.pt>
2015-11-25 00:17:09 +0000

    Fixed input
    Input, mutation and output works

--- lab6/src/exercise/Simulator.java
+++ lab6/src/exercise/Simulator.java
@@ -2,13 +2,16 @@ package exercise;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 public class Simulator {
 	
@@ -20,7 +23,7 @@ public class Simulator {
 	private double mr; // mutation rate
 	private double rr; // recombination rate
 	private int rfl; // recombination fragment length
-	private List<String> sequences;
+	private List<String> sequences; //list of sequences from input file
 	
 	public static void main(String[] args) {
 		
@@ -31,23 +34,38 @@ public class Simulator {
 		//sim.recombination();
 		sim.output();
 	}
-	
+		
 	// function to process input from FASTA file
-	private void input(){		
+	private void input(){
+		String[] parsed = null;
+		String[] splited = null;
+		sequences = new ArrayList <String>();		
+		
+		// to create interactive window to input FASTA file
+		File sf = null;
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+		int result = fileChooser.showOpenDialog(fileChooser);
+		if (result == JFileChooser.APPROVE_OPTION) {
+		    sf = fileChooser.getSelectedFile();		    
+		}
+		
 		StringBuffer stringBuffer = new StringBuffer();
 		String line = null;
 		
-		System.out.println("Welcome to Simulator!");
+		//System.out.println("Welcome to Simulator!");
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Introduce FASTA file ->");
-		this.INFILENAME = in.nextLine();
+		//System.out.println("Introduce FASTA file ->");
+		//this.INFILENAME = in.nextLine();
+		this.INFILENAME = sf.getAbsolutePath();
+		//System.out.println(INFILENAME);
 		
-		System.out.println("Introduce number of sequence size ->");
-		this.ss = in.nextInt();
+		//System.out.print("Introduce number of sequence size ->");
+		//this.ss = in.nextInt();
 		
-		System.out.println("Introduce number of population size ->");
-		this.ps = in.nextInt();
+		//System.out.print("Introduce number of population size ->");
+		//this.ps = in.nextInt();
 		
 		try {
 			FileReader reader = new FileReader(INFILENAME);
@@ -56,25 +74,17 @@ public class Simulator {
 			// in each line at FASTA file we append new line
 			while((line = bufferedreader.readLine())!=null){
 				stringBuffer.append(line).append("\n");
-				}
-			
-			// to remove initial header
-			String splited[] = (stringBuffer.toString()).split("\\n",2);
-			
-			// to remove \n from original genome in FASTA file
-			splited[1] = splited[1].replaceAll("[\r\n]", "");
-			
-			// to construct each sequences from genome at FASTA file
-			this.sequences = new ArrayList<String>();
-			int index = 0;
-			while (index < splited[1].length()) {
-			    this.sequences.add(splited[1].substring(index, Math.min(index + this.ss,splited[1].length())));
-			    index += this.ss;
+				parsed = (stringBuffer.toString()).split(">");
 			}
-			/*
-			for(int i = 0; i<this.ps; i++)
-			System.out.println(">sequence_"+(i+1)+"\n"+this.sequences.get(i)+"\n");
-			*/
+			
+			//to remove header from FASTA file 
+			for(int i = 1; i<parsed.length; i++){
+				splited = parsed[i].split("\\n",2);
+				String sequence = splited[1].replaceAll("[\r\n]", "");
+				this.sequences.add(sequence);
+				//System.out.println("lista: "+ this.sequences.get(i-1));
+			}
+			
 			reader.close();
 			
 		} catch (IOException e) {
@@ -83,77 +93,81 @@ public class Simulator {
 	}
 	
 	//Mutation
-	private void mutation(){
+		private void mutation(){
+			
+			String seq;
+			String seqMutated;
+			int pos;
+			char oldNucleotide;
+			char newNucleotide;
+
+			Random rand = new Random();
+
+			//1st step
+			for(int i = 0; i < sequences.size(); i++) {
 		
-		String seq;
-		String seqMutated;
-		int pos;
-		char oldNucleotide;
-		char newNucleotide;
+				double number = rand.nextDouble(); 
 
-		Random rand = new Random();
-
-		//1st step
-		for(int i = 0; i < sequences.size(); i++) {
-	
-			double number = rand.nextDouble(); 
-
-			//2nd step
-			if(number <= mr){ //condition to mutate
-				seq = sequences.get(i);
-				pos = rand.nextInt(seq.length()-1); //choose a random position in the sequence
-				
-				oldNucleotide = seq.charAt(pos); //get the actual nucleotide
-				newNucleotide = randomNucleotide(oldNucleotide); //choose a random nucleotide           
-				
-				seqMutated = seq.substring(0,pos) + newNucleotide + seq.substring(pos+1); //change the actual nucleotide, in the random position, to a different one 
-				
-				//3rd step
-				sequences.set(i, seqMutated); //update the sequence
-			} 
-		}
-	}
-
-	//Random nucleotide generator
-	public char randomNucleotide(char nucleotide){ 
-		
-		int rand;
-		char newNucleotide = nucleotide;
-
-		Random generator = new Random();
-		rand = generator.nextInt(3);
-
-		while(newNucleotide == nucleotide){ //guarantees that the new nucleotide is different from the original
-			switch (rand) {
-				case 0 : newNucleotide = 'A';
-						 break;
-				case 1 : newNucleotide = 'C';
-						 break;
-				case 2 : newNucleotide = 'T';
-						 break;
-				case 3 : newNucleotide = 'G';
-						 break;
+				//2nd step
+				if(number <= mr){ //condition to mutate
+					seq = sequences.get(i);
+					pos = rand.nextInt(seq.length()-1); //choose a random position in the sequence
+					
+					oldNucleotide = seq.charAt(pos); //get the actual nucleotide
+					newNucleotide = randomNucleotide(oldNucleotide); //choose a random nucleotide           
+					
+					seqMutated = seq.substring(0,pos) + newNucleotide + seq.substring(pos+1); //change the actual nucleotide, in the random position, to a different one 
+					
+					//3rd step
+					sequences.set(i, seqMutated); //update the sequence
+				} 
 			}
-			rand = generator.nextInt(3);
 		}
-		return newNucleotide;
-	}
 
+		//Random nucleotide generator
+		public char randomNucleotide(char nucleotide){ 
+			
+			int rand;
+			char newNucleotide = nucleotide;
+
+			Random generator = new Random();
+			rand = generator.nextInt(3);
+
+			while(newNucleotide == nucleotide){ //guarantees that the new nucleotide is different from the original
+				switch (rand) {
+					case 0 : newNucleotide = 'A';
+							 break;
+					case 1 : newNucleotide = 'C';
+							 break;
+					case 2 : newNucleotide = 'T';
+							 break;
+					case 3 : newNucleotide = 'G';
+							 break;
+				}
+				rand = generator.nextInt(3);
+			}
+			return newNucleotide;
+		}
+
+	
 	private void output(){
+		System.out.println(this.sequences);
+		System.out.println(this.sequences.get(0));
 		try {
 			
-			// create a FASTA file with output on desired folder
-			Scanner out = new Scanner(System.in);
-			System.out.println("Where you want to save the FASTA file? ->");
-			this.OUTFILENAME = out.nextLine();
+			int endIndex = this.INFILENAME.lastIndexOf("\\");
+		    if (endIndex != -1)  
+		    {
+		       this.OUTFILENAME = this.INFILENAME.substring(0, endIndex); // not forgot to put check if(endIndex != -1)
+		    }
 			
-			FileWriter writer = new FileWriter(OUTFILENAME + "/output.fasta");
+		    // to save output file in the same folder than input
+			FileWriter writer = new FileWriter(this.OUTFILENAME + "/output5.fasta");
 			BufferedWriter bufferedWriter = new BufferedWriter(writer);
 			
-			bufferedWriter.write("Dummy header fix it later ... or not...\n");
-			
-			// to write each sequence in a different line on FASTA file
-			for(int i = 0; i<this.ps; i++){
+			// to write each sequence in FASTA file
+			for(int i = 0; i<this.sequences.size(); i++){
+				bufferedWriter.write(">Sequence"+"_"+(i+1)+"\n");
 				bufferedWriter.write(this.sequences.get(i));
 				bufferedWriter.write("\n");
 			}
@@ -164,4 +178,7 @@ public class Simulator {
 			e.printStackTrace();
 		}
 	}
+
 }
+
+
